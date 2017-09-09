8bbce766db643d39b722a7d5ed0a58d2aaa3f391
Kieran Williams <kieran.williams@digipen.edu>
2017-09-08 18:30:14 -0700

    Imported graphics files into the engine.

---
+++ game/src/graphics/Settings.cpp
@@ -0,0 +1,8 @@
+#include "Settings.h"
+
+#define SCREEN_WIDTH  800
+#define SCREEN_HEIGHT 600
+
+int Settings::ScreenWidth() { return SCREEN_WIDTH; }
+int Settings::ScreenHeight() { return SCREEN_HEIGHT; }
+int Settings::AntiAliasing() { return 4; }
