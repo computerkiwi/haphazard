b76ac9b98d190c116190bcd6a8408dd0da8f4880
Kieran Williams <kieran.williams@digipen.edu>
2017-09-12 23:12:20 -0700

    Added a universal header for stuff used everywhere.
    Currently only contains logging. Should be made precompiled at some point.

---
+++ game/src/Universal.h
@@ -0,0 +1,13 @@
+/*
+FILE: GameSpace.h
+PRIMARY AUTHOR: Kieran
+
+General universal header containing things that will commonly be used everywhere.
+
+Copyright © 2017 DigiPen (USA) Corporation.
+*/
+#pragma once
+
+// TODO - Make this a precompiled header.
+
+#include "Util/Logging.h"
