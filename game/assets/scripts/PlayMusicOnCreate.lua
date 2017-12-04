95c88ca0a9b657b04692080b4e5042cd565e7a4f
Unknown <kieran.williams@digipen.edu>
2017-12-04 15:13:53 -0800

    Fixed a small bug with the music script.

--- game/assets/scripts/PlayMusicOnCreate.lua
+++ game/assets/scripts/PlayMusicOnCreate.lua
@@ -5,7 +5,7 @@ VOLUME = 0.4
 SONG_NAME = "level_1.wav"
 
 function Start()
-	if (_G.currentMusic == nil or _G.currentMusic.GetFileName() ~= SONG_NAME)
+	if (_G.currentMusic == nil or _G.currentMusic:GetFileName() ~= SONG_NAME)
 	then
 		_G.currentMusic = PlaySound(SONG_NAME, VOLUME, 1, true)
 	end
