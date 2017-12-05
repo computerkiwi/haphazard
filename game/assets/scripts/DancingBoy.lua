052b275c44313f8e67263f75d97f9ca4867b29fa
Maxr00 <moxrauffer@aol.com>
2017-12-04 16:41:39 -0800

    Auto stash before merge of "master" and "origin/master"

--- game/assets/scripts/DancingBoy.lua
+++ game/assets/scripts/DancingBoy.lua
@@ -8,15 +8,50 @@ Copyright (c) 2017 DigiPen (USA) Corporation.
 DANCE_TIME = 0.35
 time = 0.35
 
+startYScale = 0
+standingY = 0
+startY = 0
+delta = 0
+
+inAir = true
+
+function Start()
+
+	startYScale = this:GetTransform().scale.y
+
+	--DANCE_TIME = math.random() + 0.1
+	time = DANCE_TIME
+	standingY = this:GetTransform().position.y
+
+end
+
 
 function Update(dt)
-	time = time - dt
+	if(dt == 0) then return	end
 	
-	this:GetTransform().scale = vec3(this:GetTransform().scale.x, 1.25 + 0.25 * math.cos(6.28 / DANCE_TIME * time), 1)
+	time = time - dt
+	local delta = 0.25 * math.cos(6.28 / DANCE_TIME *(time));
 
-	this:GetTransform().position = vec2(this:GetTransform().position.x, 0.13 + 0.25/2 * math.cos(6.28 / DANCE_TIME *time))
+	this:GetTransform().scale = vec3(this:GetTransform().scale.x, startYScale + delta, 1)
 
-	if(time < 0)
+	if(this:GetRigidBody().velocity.y != 0) 
+	then 
+		inAir = true 
+	end
+
+	if(inAir)
+	then
+		if(this:GetRigidBody().velocity.y == 0)
+		then
+			inAir = false;
+			standingY = this:GetTransform().position.y
+		end
+	else
+		this:GetTransform().position = vec2(this:GetTransform().position.x, standingY + delta)
+	end
+
+
+	if(time < -DANCE_TIME)
 	then
 		this:GetTransform().scale = vec3(-this:GetTransform().scale.x, this:GetTransform().scale.y, 1)
 		time = DANCE_TIME
