a11877fc1a9c1f398dd270e1537b9a9060837aae
Brett Schiff <brettschiff@gmail.com>
2017-12-04 18:36:16 -0800

    Cheat codes that teleport to objects name "CheatTeleportX", where X is a number between 1 and 5

---
+++ game/assets/scripts/CheatTeleport.lua
@@ -0,0 +1,58 @@
+--[[
+FILE: CheatTeleport1.lua
+PRIMARY AUTHOR: Brett Schiff
+
+Copyright (c) 2017 DigiPen (USA) Corporation.
+]]
+
+-- Updates each frame
+function Update(dt)
+
+	
+	local obj1 = GameObject.FindByName("CheatTeleport1")
+	local obj2 = GameObject.FindByName("CheatTeleport2")
+	local obj3 = GameObject.FindByName("CheatTeleport3")
+	local obj4 = GameObject.FindByName("CheatTeleport4")
+	local obj5 = GameObject.FindByName("CheatTeleport5")
+
+	if(IsPressed(KEY.LeftControl) and IsTriggered(KEY.One) and obj1:IsValid())
+	then
+
+		local location = obj1:GetTransform().position
+		this:GetTransform().position = location
+
+	end
+
+	if(IsPressed(KEY.LeftControl) and IsTriggered(KEY.Two) and obj2:IsValid())
+	then
+
+		local location = obj2:GetTransform().position
+		this:GetTransform().position = location
+
+	end
+
+	if(IsPressed(KEY.LeftControl) and IsTriggered(KEY.Three) and obj3:IsValid())
+	then
+
+		local location = obj3:GetTransform().position
+		this:GetTransform().position = location
+
+	end
+
+	if(IsPressed(KEY.LeftControl) and IsTriggered(KEY.Four) and obj4:IsValid())
+	then
+
+		local location = obj4:GetTransform().position
+		this:GetTransform().position = location
+
+	end
+
+	if(IsPressed(KEY.LeftControl) and IsTriggered(KEY.Five) and obj5:IsValid())
+	then
+
+		local location = obj5:GetTransform().position
+		this:GetTransform().position = location
+
+	end
+
+end -- fn end
