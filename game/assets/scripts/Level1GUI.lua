c0f7d0ddb61785327f6bac769c4d6daba18d18ba
Maxr00 <moxrauffer@aol.com>
2017-12-05 18:09:06 -0800

    Level1 Gui and health animations, resized window

---
+++ game/assets/scripts/Level1GUI.lua
@@ -0,0 +1,36 @@
+--[[
+FILE: ParentToCamera.lua
+PRIMARY AUTHOR: Max Rauffer
+
+Copyright (c) 2017 DigiPen (USA) Corporation.
+]]
+
+cam = nil
+
+offset = vec2(0,0)
+startScale = vec3(0,0,0)
+
+function Start()
+	cam = GameObject.FindByName("Main Camera");
+
+	if(this:GetName() == "Player1Healthbar")
+	then
+		offset = vec2(-2,2)
+	elseif(this:GetName() == "Player2Healthbar")
+	then
+		offset = vec2(2,2)
+	elseif(this:GetName() == "GemCounter")
+	then
+		offset = vec2(0,2)
+	end
+
+	startScale = this:GetTransform().scale
+	--offset = this:GetTransform().position
+
+end
+
+function Update(dt)
+	local zoomScale = cam:GetCamera().zoom / 5
+	this:GetTransform().position = vec2(cam:GetTransform().position.x + offset.x * zoomScale, cam:GetTransform().position.y + offset.y * zoomScale)
+	this:GetTransform().scale = vec3(startScale.x * zoomScale, startScale.y * zoomScale, 1)
+end
