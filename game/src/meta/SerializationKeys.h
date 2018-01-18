f19ef5e0df35011522706cdfe789a669eeac47a9
Kieran Williams <kieranw.gamedev@gmail.com>
2018-01-18 15:21:25 -0800

    Shortened some serialization keywords.

---
+++ game/src/meta/SerializationKeys.h
@@ -0,0 +1,34 @@
+/*
+FILE: SerializationKeys.h
+PRIMARY AUTHOR: Kieran Williams
+
+Copyright (c) 2018 DigiPen (USA) Corporation.
+*/
+#pragma once
+
+#include <vector>
+
+const std::vector<const char *> META_TYPE_NAME =
+{
+	"nam",
+	"meta_type_name"
+};
+
+const std::vector<const char *> META_OBJECT_DATA =
+{
+	"dat",
+	"meta_object_data"
+};
+
+template <typename JsonType>
+auto& GetMemberBackwardsCompatible(JsonType& obj, const std::vector<const char *>& compatabilityVector)
+{
+	for (const char *key : compatabilityVector)
+	{
+		if (obj.HasMember(key))
+		{
+			return obj[key];
+		}
+	}
+}
+
