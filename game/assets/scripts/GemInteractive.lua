c906a1e29fb5daa964ba2714ffd216bb217215b7
Unknown <lya.vera@gmail.com>
2018-01-29 13:06:31 -0800

    Level 2 edits

---
+++ game/assets/scripts/GemInteractive.lua
@@ -0,0 +1,95 @@
+--[[
+FILE: GemInteractive.lua
+PRIMARY AUTHOR: Lya Vera
+
+Copyright (c) 2017 DigiPen (USA) Corporation.
+]]
+
+startPos = 0
+endPos = 0
+percent = 0.10
+distance = -10 -- Distance to move
+steps = 1 -- How fast it moves
+moveEnabled = false
+moveDir = 0
+moveAxis = 0
+
+-- Enums
+UP = 1
+DOWN = -1
+LEFT = -1
+RIGHT = 1
+
+function Start()
+  -- Make sure object is active
+  this:Activate()
+  -- Save the start position
+  startPos = this:GetTransform().position
+  endPos = this:GetTransform().position
+  endPos.y = endPos.y + distance
+  moveDir = DOWN
+
+end -- fn end
+
+function Update(dt)
+  if(moveEnabled)
+  then
+    print("Attempting to move")
+
+    local transform = this:GetTransform()
+    local currPos = transform.position
+
+    currPos.y = currPos.y + (steps * moveDir)
+
+    transform.position = currPos
+    
+    if (currPos.y == endPos.y or currPos.y < endPos.y)
+    then
+      moveEnabled = false
+    end
+  end
+
+  if (IsPressed(KEY.Enter))
+  then
+    Reset()
+  end
+end -- fn end
+
+function OnCollisionEnter(other)
+  -- Player collision
+  if (other:HasTag("Player"))
+  then
+    print("Move enabled")
+    moveEnabled = true
+  end
+end -- fn end
+
+-- Public function to be called
+function PlaceGem()
+
+end -- fn end
+
+--[[function SetMoveDir(axis, dir)
+  
+  if (axis == "Horizontal")
+  then
+    axis = this:GetTransform().position.x
+  elseif (axis == "Vertical")
+    axis = this:GetTransform().position.y
+  end
+
+  if (dir == 1)
+  then
+    moveDir = 1
+  elseif (dir == -1)
+    moveDir = -1
+  end
+
+end]] -- fn end
+
+-- Reset position
+function Reset()
+  local transform = this:GetTransform()
+  transform.position = startPos
+  moveEnabled = false
+end --fn end
