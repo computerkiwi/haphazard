cc1c962b6e1e2b528fe1a436f20d56310bfef2b6
Unknown <lya.vera@gmail.com>
2018-02-05 19:26:56 -0800

    Added challenge script

---
+++ game/assets/scripts/ChallengeScript.lua
@@ -0,0 +1,29 @@
+--[[
+FILE: ChallengeScript.lua
+PRIMARY AUTHOR: Lya Vera
+
+Copyright (c) 2017 DigiPen (USA) Corporation.
+]]
+
+function Start()
+  this:Activate()
+end -- fn end
+
+function OnCollisionEnter(other)
+  -- Player takes coin
+  if (other:HasTag("Player"))
+  then
+
+  
+	--PlaySound("regular_pickup.mp3", 0.2, 1, false)
+
+--    local score = GameObject.FindByName("Stats")
+
+    -- Increment score
+--    local script = score:GetScript("PlayerStats.lua")
+--    script.AddScore()
+
+    -- Deactivate the object
+    this:Deactivate()
+  end
+end -- fn end
