1b395e3ff17c47f9c309af3482ae0de103cd3eb2
Kieran Williams <kieran.williams@digipen.edu>
2018-02-07 20:22:01 -0800

    Made emotes work.

--- game/assets/scripts/EmoteSpawner.lua
+++ game/assets/scripts/EmoteSpawner.lua
@@ -4,37 +4,20 @@ PRIMARY AUTHOR: Ash Duong
 
 Copyright (c) 2018 DigiPen (USA) Corporation.
 ]]
-  
+
 function Start()
-	local p1emotes = GameObject.LoadPrefab("assets/prefabs/Emotes.json")
-	p1emotes:GetTransform().parent = GameObject.FindByName("Player1")
-	p1emotes:GetScript("Emotes.lua").Player = p1emotes:GetTransform().parent
-	local newpos1 = p1emotes:GetTransform().position
-	newpos1.x = GameObject.FindByName("Player1"):GetTransform().position.x + 0.5
-	newpos1.y = GameObject.FindByName("Player1"):GetTransform().position.y + 0.5
-	p1emotes:GetTransform().position = newpos1
+	-- Remove an emote object if it exists.
+	local nameString = "__Emote_Obj_" .. this:GetName()
+	local tempObj = GameObject.FindByName(nameString)
+	if (tempObj:IsValid())
+	then
+		tempObj:Destroy()
+	end
 
-	local p2emotes = GameObject.LoadPrefab("assets/prefabs/Emotes.json")
-	p2emotes:GetTransform().parent = GameObject.FindByName("Player2")
-	p2emotes:GetScript("Emotes.lua").Player = p2emotes:GetTransform().parent
-	local newpos2 = p2emotes:GetTransform().position
-	newpos2.x = GameObject.FindByName("Player2"):GetTransform().position.x + 0.5
-	newpos2.y = GameObject.FindByName("Player2"):GetTransform().position.y + 0.5
-	p2emotes:GetTransform().position = newpos2
-
-	local p3emotes = GameObject.LoadPrefab("assets/prefabs/Emotes.json")
-	p3emotes:GetTransform().parent = GameObject.FindByName("Player3")
-	p3emotes:GetScript("Emotes.lua").Player = p3emotes:GetTransform().parent
-	local newpos3 = p3emotes:GetTransform().position
-	newpos3.x = GameObject.FindByName("Player3"):GetTransform().position.x + 0.5
-	newpos3.y = GameObject.FindByName("Player3"):GetTransform().position.y + 0.5
-	p3emotes:GetTransform().position = newpos3
-
-	local p4emotes = GameObject.LoadPrefab("assets/prefabs/Emotes.json")
-	p4emotes:GetTransform().parent = GameObject.FindByName("Player4")
-	p4emotes:GetScript("Emotes.lua").Player = p4emotes:GetTransform().parent
-	local newpos4 = p4emotes:GetTransform().position
-	newpos4.x = GameObject.FindByName("Player4"):GetTransform().position.x + 0.5
-	newpos4.y = GameObject.FindByName("Player4"):GetTransform().position.y + 0.5
-	p4emotes:GetTransform().position = newpos4
+	
+	local emote = GameObject.LoadPrefab("assets/prefabs/Emotes.json")
+	emote:SetName(nameString)
+	emote:GetTransform().parent = this
+	emote:GetScript("Emotes.lua").Player = this
+	emote:GetTransform().localPosition = vec2(0.5, 0.5)
 end
