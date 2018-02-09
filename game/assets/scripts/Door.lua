17a3dc888d06d08b95c021aa22d212f18556bdd0
Maxr00 <moxrauffer@aol.com>
2018-02-08 18:01:59 -0800

    Fixed door script

--- game/assets/scripts/Door.lua
+++ game/assets/scripts/Door.lua
@@ -6,29 +6,14 @@ Copyright (c) 2017 DigiPen (USA) Corporation.
 ]]--
 
 DESTINATION_LEVEL		= "MainMenu.json"	
-GNOME_HERE 				= false
-CURRENT_GNOME 			= nil
-
-function Update(dt)
-	if((GNOME_HERE == true) and (CURRENT_GNOME ~= nil))
-	then
-		if(CURRENT_GNOME:GetScript("InputHandler.lua").jumpPressed)
-		then
-			Engine.LoadLevel(DESTINATION_LEVEL)
-		end
-	end
-
-end
+isColliding = false
 
 function OnCollisionEnter(other)
 	if(other:HasTag("Player"))
 	then
-	GNOME_HERE = true
-	CURRENT_GNOME = other
+		if(other:GetScript("InputHandler.lua").jumpPressed)
+		then
+			Engine.LoadLevel(DESTINATION_LEVEL)
+		end
 	end
-end
-
-function OnCollisionExit(other)
-
-	GNOME_HERE = false
 end
