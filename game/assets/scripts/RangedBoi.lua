4846506268caa097c1f8d704302e22b1f4bf23cd
Brett Schiff <brettschiff@gmail.com>
2018-02-23 18:41:57 -0800

    RangedBoi functionality
    Just need to add real math for projectile aiming

--- game/assets/scripts/RangedBoi.lua
+++ game/assets/scripts/RangedBoi.lua
@@ -16,41 +16,152 @@ GROUND_LAYER = 1 << 3
 lookDirectionDegrees = 180
 lookConeWidth = 45
 lookConeLength = 5
-lookNumRays = 4
+lookNumRays = 5
 
 -- info about shooting
-firstShotDelay = 0
-secondShotDelay = .2
-thirdShotDelay = .2
-interFiringPatternDelay = 1
+numberOfShotsPerPattern = 3
+timeBetweenShots = 1
+
+timeBetweenFiringPatterns = 5
+
+currTime = 0
+currentlyInFiringPattern = false
+shotsFired = 0
+
+target = GameObject(0)
+
+projectilePrefabName =  "RangedBoiProjectile.json"
+projectileLaunchAngleDegrees = 45
 
 function LookForGnomes()
 
 	-- start downward and adjust according to the cone width
-	local initialDirection = lookDirectionDegrees + (lookConeWidth / 2)
+	local initialDirection = lookDirectionDegrees - (lookConeWidth / 2)
 	local directionIncrease = lookConeWidth / (lookNumRays - 1)
-	local endDirection = lookDirectionDegrees - (lookConeWidth / 2)
+	local endDirection = lookDirectionDegrees + (lookConeWidth / 2)
 
 	-- raycast a number of times in the correct directions
 	for currDirection = initialDirection, endDirection, directionIncrease
 	do
-		 -- raycast from the bird
-		 local cast = Raycast.RaycastAngle(this:GetSpaceIndex(), this:GetTransform().position, currDirection, lookConeLength, PLAYER1LAYER | PLAYER2LAYER | PLAYER3LAYER | PLAYER4LAYER | GROUND_LAYER)
+		-- raycast from itself
+		local cast = Raycast.RaycastAngle(this:GetSpaceIndex(), this:GetTransform().position, currDirection, lookConeLength, PLAYER1LAYER | PLAYER2LAYER | PLAYER3LAYER | PLAYER4LAYER | GROUND_LAYER)
 
-		 if(cast.gameObjectHit:IsValid() and cast.gameObjectHit:GetCollider().collisionLayer.layer ~= GROUND_LAYER)
-		 then
+		-- if a gnome was hit
+		if(cast.gameObjectHit:IsValid() and cast.gameObjectHit:GetCollider().collisionLayer.layer ~= GROUND_LAYER)
+		then
 
+			print("Rangedboi: Rangedboi saw a gnome")
+			currentlyInFiringPattern = true;
+			currTime = timeBetweenShots
+			shotsFired = 0
 			return cast.gameObjectHit
 
-		 end
+		end
 
 	end
 
+	-- no gnome was found
+	return GameObject(0)
+
 end
 
-function ShootAtTarget()
-
+function ShootAtTarget(target)
 	
+	-- spawn a projectile and get its info
+	local projectile = GameObject.LoadPrefab("assets/prefabs/" .. projectilePrefabName)
+
+	-- get the vector to the target
+	targetPosition = target:GetTransform().position
+	thisPosition = this:GetTransform().position
+	vecToTarget = vec2(targetPosition.x - thisPosition.x, targetPosition.y - thisPosition.y)
+	
+	-- set the position of the projectile
+	yPosition = thisPosition.y
+	xPosition = thisPosition.x
+	
+	if(vecToTarget.x <= 0)
+	then
+
+		xPosition = xPosition - (this:GetCollider().dimensions.x / 2) - projectile:GetCollider().dimensions.x
+
+	else
+	
+		xPosition = xPosition + (this:GetCollider().dimensions.x / 2) + projectile:GetCollider().dimensions.x
+
+	end
+
+	projectilePosition = vec2(xPosition, yPosition)
+	projectile:GetTransform().position = projectilePosition
+
+	-- set the velocity of the projectile
+	projectile:GetRigidBody().velocity = vec3(vecToTarget.x, 2, 0)
+
+end
+
+function ShootAtCorrectTime(dt)
+
+	-- if the enemy is between firing patterns
+	if(currentlyInFiringPattern == false)
+	then
+		-- if the time has passed to go back into a firing pattern
+		if(currTime >= timeBetweenFiringPatterns)
+		then
+			
+			currentlyInFiringPattern = true
+			shotsFired = 0
+			currTime = currTime - timeBetweenFiringPatterns + timeBetweenShots
+			return
+
+		else -- continue waiting till the next firing pattern
+
+			currTime = currTime + dt
+			return
+
+		end
+
+	else -- the enemy is in a firing pattern
+
+		if(currTime >= timeBetweenShots)
+		then
+
+			currTime = currTime - timeBetweenShots
+			shotsFired = shotsFired + 1
+
+			-- if the firing pattern has been completed, break
+			if(shotsFired >= numberOfShotsPerPattern)
+			then
+
+				currentlyInFiringPattern = false
+				shotsFired = 0
+
+			end
+
+		else -- continue waiting till the next shot
+
+			currTime = currTime + dt
+			return
+
+		end
+	end
+
+	-- Raycast at the gnome to make sure it's still in vision
+	directionTowardsTarget = vec2(- this:GetTransform().position.x + target:GetTransform().position.x, - this:GetTransform().position.y + target:GetTransform().position.y)
+
+	theCast = Raycast.Cast(this:GetSpaceIndex(), this:GetTransform().position, directionTowardsTarget, lookConeLength, PLAYER1LAYER | PLAYER2LAYER | PLAYER3LAYER | PLAYER4LAYER | GROUND_LAYER)
+
+	if(theCast.gameObjectHit:IsValid() and theCast.gameObjectHit:GetCollider().collisionLayer.layer ~= GROUND_LAYER)
+	then
+		
+		ShootAtTarget(theCast.gameObjectHit)
+
+	else
+
+		currentlyInFiringPattern = false
+		currTime = timeBetweenShots
+		shotsFired = 0
+		target = GameObject(0)
+
+	end
 
 end
 
@@ -65,11 +176,22 @@ end
 -- Updates each frame
 function Update(dt)
 	
-	-- look for gnomes
-	local targetGnome = LookForGnomes()
+	-- if there is no valid target
+	if(not target:IsValid())
+	then
 
-	-- shoot at the target gnomes
-	ShootAtTarget(targetGnome)
+		-- look for gnomes
+		target = LookForGnomes()
+
+	else
+
+		-- shoot at the target gnome
+		ShootAtCorrectTime(dt)
+
+	end
+	
+
+	
 
 end
 
