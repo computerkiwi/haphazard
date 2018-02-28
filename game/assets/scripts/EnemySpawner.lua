d3c2a397661e94f177d9608c3de965025002938b
Unknown <lya.vera@gmail.com>
2018-02-27 17:43:41 -0800

    Enemy spawner implemented in DeathPit

--- game/assets/scripts/EnemySpawner.lua
+++ game/assets/scripts/EnemySpawner.lua
@@ -5,8 +5,59 @@ PRIMARY AUTHOR: Lya Vera
 Copyright (c) 2017 DigiPen (USA) Corporation.
 ]]
 
---ENEMY = "assets/prefabs/ (TODO: add shroom prefab; use the tiny shroom sprite)
+ENEMY = "assets/prefabs/Creepyboi.json"
 
-function Update()
+Enabled = true -- Enabled the spawner
+SpawnTimerMax = 3.2
+SpawnTimer = SpawnTimerMax
+SpawnTotalMax = 5  -- Total number of enemies to spawn
+SpawnTotal = SpawnTotalMax
 
+function Start()
+  this:Active()
+end -- fn end
+
+function Update(dt)
+  if (IsHeld(KEY.NumpadAdd))
+  then
+    Enabled = Enabled or true
+    print(Enabled)
+  end
+
+  if (Enabled)
+  then
+    SpawnTimer = SpawnTimer - dt
+    if (SpawnTimer <= 0)
+    then
+      SpawnEnemy()
+      SpawnTimer = SpawnTimerMax
+    end
+  end
+end -- fn end
+
+-- Disable the spawner
+function DisableSpawner()
+  Enabled = false
+end -- fn end
+
+-- Enable the spawner
+function EnableSpawner()
+  Enabled = true
+end -- fn end
+
+function SpawnEnemy()
+  -- Spawner is enabled and can spawn enemies
+  if (SpawnTotal > 0)
+  then
+    local enemy = GameObject.LoadPrefab(ENEMY)
+    SpawnTotal = SpawnTotal - 1
+
+    enemy:GetTransform().position = this:GetTransform().position
+
+  -- Disable timer when all enemies have been spawned
+  elseif (SpawnTotal == 0)
+  then
+    Enabled = false
+    SpawnTotal = SpawnTotalMax
+  end
 end -- fn end
