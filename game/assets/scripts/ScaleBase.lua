b392628b2675d7f41d49e6adc13b35ccf1b633d5
Unknown <kieranw.gamedev@gmail.com>
2018-02-28 23:45:21 -0800

    Rope and visual improvements.

--- game/assets/scripts/ScaleBase.lua
+++ game/assets/scripts/ScaleBase.lua
@@ -17,6 +17,10 @@ gnomeCounterObjRight = nil
 gnomeCounterLeft = nil
 gnomeCounterRight = nil
 
+ropeLeft = nil
+ropeMid = nil
+ropeRight = nil
+
 pulleyStartOffsetLeft_x = -3
 pulleyStartOffsetLeft_y = 0
 pulleyStartOffsetRight_x = 3
@@ -60,56 +64,89 @@ function SpawnAndAttachObject(prefabName, parentObj)
 	end
 end
 
+function PositionRope(rope, pos1, pos2)
+	-- Calculate the angle
+	local diff = {x = pos1.x - pos2.x, y = pos1.y - pos2.y}
+	local length = math.sqrt(diff.x * diff.x + diff.y * diff.y)
+	local mid = vec2((pos1.x + pos2.x) / 2, (pos1.y + pos2.y) / 2)
+	
+	local transform = rope:GetTransform()
+	
+	-- Move to the right position
+	transform.position = mid
+	
+	-- Stretch to touch both objects.
+	local tempScale = transform.scale
+	tempScale.y = length
+	transform.scale = tempScale
+	
+	-- Rotate along the diff vector.
+	local angle = math.deg(math.atan(diff.y, diff.x)) - 90
+	transform.rotation = angle
+end
+
 function Start()
-  -- Spawn all the objects we need.
-  pulleyLeft = SpawnAndAttachObject("assets/prefabs/scale_internal/Pulley.json", this)
-  pulleyLeft:GetTransform().localPosition = vec2(pulleyStartOffsetLeft_x, pulleyStartOffsetLeft_y)
-  platformLeft = SpawnAndAttachObject("assets/prefabs/scale_internal/ScalePlatform.json", pulleyLeft)
-  gnomeCounterObjLeft = SpawnAndAttachObject("assets/prefabs/scale_internal/ScalePlatformGnomeCounter.json", platformLeft)
-  gnomeCounterObjLeft:GetTransform().localPosition = vec2(0, 0.15)
-  gnomeCounterLeft = gnomeCounterObjLeft:GetScript("GnomeCounter.lua")
-  
-  pulleyRight = SpawnAndAttachObject("assets/prefabs/scale_internal/Pulley.json", this)
-  pulleyRight:GetTransform().localPosition = vec2(pulleyStartOffsetRight_x, pulleyStartOffsetRight_y)
-  platformRight = SpawnAndAttachObject("assets/prefabs/scale_internal/ScalePlatform.json", pulleyRight)
-  gnomeCounterObjRight = SpawnAndAttachObject("assets/prefabs/scale_internal/ScalePlatformGnomeCounter.json", platformRight)
-  gnomeCounterObjRight:GetTransform().localPosition = vec2(0, 0.15)
-  gnomeCounterRight = gnomeCounterObjRight:GetScript("GnomeCounter.lua")
-  
-  leftRopeLength = startingLeftRopeLength
-  UpdatePlatformPositions()
+	-- Spawn all the objects we need.
+	pulleyLeft = SpawnAndAttachObject("assets/prefabs/scale_internal/Pulley.json", this)
+	pulleyLeft:GetTransform().localPosition = vec2(pulleyStartOffsetLeft_x, pulleyStartOffsetLeft_y)
+	platformLeft = SpawnAndAttachObject("assets/prefabs/scale_internal/ScalePlatform.json", pulleyLeft)
+	gnomeCounterObjLeft = SpawnAndAttachObject("assets/prefabs/scale_internal/ScalePlatformGnomeCounter.json", platformLeft)
+	gnomeCounterObjLeft:GetTransform().localPosition = vec2(0, 0.15)
+	gnomeCounterLeft = gnomeCounterObjLeft:GetScript("GnomeCounter.lua")
+
+	pulleyRight = SpawnAndAttachObject("assets/prefabs/scale_internal/Pulley.json", this)
+	pulleyRight:GetTransform().localPosition = vec2(pulleyStartOffsetRight_x, pulleyStartOffsetRight_y)
+	platformRight = SpawnAndAttachObject("assets/prefabs/scale_internal/ScalePlatform.json", pulleyRight)
+	gnomeCounterObjRight = SpawnAndAttachObject("assets/prefabs/scale_internal/ScalePlatformGnomeCounter.json", platformRight)
+	gnomeCounterObjRight:GetTransform().localPosition = vec2(0, 0.15)
+	gnomeCounterRight = gnomeCounterObjRight:GetScript("GnomeCounter.lua")
+
+	ropeLeft = GameObject.LoadPrefab("assets/prefabs/scale_internal/Rope.json")
+	ropeMid = GameObject.LoadPrefab("assets/prefabs/scale_internal/Rope.json")
+	ropeRight = GameObject.LoadPrefab("assets/prefabs/scale_internal/Rope.json")
+
+	leftRopeLength = startingLeftRopeLength
+	UpdatePlatformPositions()
 end
 
 function CalculatePlatforms(dt)
-  local diff = gnomeCounterLeft.gnomeCount - gnomeCounterRight.gnomeCount
-  -- The amount of gnomes on the gnome 
-  local numHeavyGnomes = math.max(gnomeCounterLeft.gnomeCount, gnomeCounterRight.gnomeCount)
-  
-  local targetLength = math.lerp(averageRopeLength, minRopeLength, -sign(diff) * numHeavyGnomes / 4)
-  local targetVel = maxSpeed * sign(targetLength - leftRopeLength)
-  if (math.abs(targetLength - leftRopeLength) < 0.1)
-  then
-    targetVel = 0
-  end
-  
-  currentVelocity = math.lerp(currentVelocity, targetVel, 0.05)
-  leftRopeLength = leftRopeLength + dt * currentVelocity
-  
-  -- Stop the platforms at the ends.
-  if (leftRopeLength < minRopeLength)
-  then
-    leftRopeLength = minRopeLength
-    currentVelocity = math.max(0, currentVelocity)
-  elseif( leftRopeLength > averageRopeLength * 2 - minRopeLength)
-  then
-    leftRopeLength = averageRopeLength * 2 - minRopeLength
-    currentVelocity = math.min(0, currentVelocity)
-  end
+	local diff = gnomeCounterLeft.gnomeCount - gnomeCounterRight.gnomeCount
+	-- The amount of gnomes on the gnome 
+	local numHeavyGnomes = math.max(gnomeCounterLeft.gnomeCount, gnomeCounterRight.gnomeCount)
+
+	local targetLength = math.lerp(averageRopeLength, minRopeLength, -sign(diff) * numHeavyGnomes / 4)
+	local targetVel = maxSpeed * sign(targetLength - leftRopeLength)
+	if (math.abs(targetLength - leftRopeLength) < 0.1)
+	then
+		targetVel = 0
+	end
+
+	currentVelocity = math.lerp(currentVelocity, targetVel, 0.05)
+	leftRopeLength = leftRopeLength + dt * currentVelocity
+
+	-- Stop the platforms at the ends.
+	if (leftRopeLength < minRopeLength)
+	then
+		leftRopeLength = minRopeLength
+		currentVelocity = math.max(0, currentVelocity)
+	elseif( leftRopeLength > averageRopeLength * 2 - minRopeLength)
+	then
+		leftRopeLength = averageRopeLength * 2 - minRopeLength
+		currentVelocity = math.min(0, currentVelocity)
+	end
 end
 
 function UpdatePlatformPositions()
-  platformLeft:GetTransform().localPosition = vec2(0, -leftRopeLength)
-  platformRight:GetTransform().localPosition = vec2(0, -(averageRopeLength * 2 - leftRopeLength ))
+
+	-- Move the platforms to the right spots.
+	platformLeft:GetTransform().localPosition = vec2(0, -leftRopeLength)
+	platformRight:GetTransform().localPosition = vec2(0, -(averageRopeLength * 2 - leftRopeLength ))
+	
+	-- Update the ropes.
+	PositionRope(ropeLeft, pulleyLeft:GetTransform().position, platformLeft:GetTransform().position)
+	PositionRope(ropeRight, pulleyRight:GetTransform().position, platformRight:GetTransform().position)
+	PositionRope(ropeMid, pulleyLeft:GetTransform().position, pulleyRight:GetTransform().position)
+	
 end
 
 function SavePulleyOffsets()
