0a7d654647e6ba24dd6a7883fc9898cc4d1c1bb8
Brett Schiff <brettschiff@gmail.com>
2018-03-09 16:46:27 -0800

    Added the Accelerator
    The object it's on should have passthrough colliison

---
+++ game/assets/scripts/Accelerator.lua
@@ -0,0 +1,33 @@
+--[[
+FILE: EnemyBackAndForthMovement.lua
+PRIMARY AUTHOR: Brett Schiff
+
+Copyright (c) 2017 DigiPen (USA) Corporation.
+]]
+
+directionDegrees = 90
+speed = 5
+
+function Start()
+	
+end
+
+function Update(dt)
+	
+
+end
+
+function OnCollisionEnter(other)
+
+  local speedMod = .6
+  local trueSpeed = speed * speedMod
+
+  local direction = vec2(math.cos(math.rad(directionDegrees)), math.sin(math.rad(directionDegrees)))
+
+  local tempVelocity = other:GetRigidBody().velocity
+  tempVelocity.x = math.lerp((direction.x * trueSpeed), tempVelocity.x, .3)
+  tempVelocity.y = math.lerp((direction.y * trueSpeed), tempVelocity.y, .3)
+
+  other:GetRigidBody().velocity = tempVelocity
+
+end
