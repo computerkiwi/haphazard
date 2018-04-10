c99c628a08049d216e3ae6d80b18db5522e0f6ce
Maxr00 <moxrauffer@aol.com>
2018-04-10 15:48:56 -0700

    Unused scripts deleted

--- game/assets/scripts/ReceiveMessage.lua
+++
@@ -1,3 +0,0 @@
-function TestMessageFunction(a, b, c, other)
-	print("Receiving message " .. tostring(a) .. ", " .. tostring(b) .. ", " .. tostring(c) .. " from " .. tostring(other:GetName()))
-end
--- game/assets/scripts/SendMessageOnCollide.lua
+++
@@ -1,3 +0,0 @@
-function OnCollisionEnter(other)
-	other:SendMessage("TestMessageFunction", 1, 2, "three", this)
-end
